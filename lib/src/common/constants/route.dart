e3c9835f7efacd5082c0d574d61e8a6b70863501
Piter <pitercapistrano@gmail.com>
2025-04-18 19:49:00 -0300

    Criação da Rotas de Navegação

---
+++ lib/src/common/constants/route.dart
@@ -0,0 +1,8 @@
+class NamedRoutes {
+  NamedRoutes._();
+
+  static const String home = '/home';
+  static const String projects = '/projects';
+  static const String blog = '/blog';
+  static const String games = '/games';
+}

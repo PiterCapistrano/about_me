f7c7ddc41aa6ffbc0d293baa76203ab90e063969
Piter <pitercapistrano@gmail.com>
2025-04-19 00:19:50 -0300

    Update home route path in NamedRoutes class

--- lib/src/common/constants/route.dart
+++ lib/src/common/constants/route.dart
@@ -1,7 +1,7 @@
 class NamedRoutes {
   NamedRoutes._();
 
-  static const String home = '/home';
+  static const String home = '/';
   static const String projects = '/projects';
   static const String blog = '/blog';
   static const String games = '/games';

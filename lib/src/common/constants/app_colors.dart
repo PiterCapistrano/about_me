d722343e4385e3b85dd91ebfd076fa8eda470b43
Piter <pitercapistrano@gmail.com>
2025-04-19 00:20:46 -0300

    Add AppColors class with predefined color constants and gradients

---
+++ lib/src/common/constants/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color backGroudColor = Color(0xFF000000);
+  static const Color backGroudColor2 = Color.fromARGB(255, 63, 63, 63);
+  static const Color textColor = Color(0xFFFFFFFF);
+  static const Color titleColor = Color.fromARGB(255, 184, 0, 0);
+  static const Color borderColor = Color.fromARGB(255, 99, 0, 0);
+  static const Color white = Color(0xFFFFFFFF);
+
+  static const List<Color> redGradient = [
+    Color.fromARGB(255, 184, 0, 0),
+    Color.fromARGB(255, 99, 0, 0),
+  ];
+
+  static const List<Color> greyGradient = [
+    Color(0xffB5B5B5),
+    Color(0xFF7F7F7f),
+  ];
+}
